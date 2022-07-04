69f8dafbc8fddc35d06ed2dc20a030a5b5d8d71e
Fredy Sandoval <shockerovip@gmail.com>
2022-07-03 19:26:00 -0600

    seventh

--- docs/index.0c7be86b.js
+++
@@ -1,2 +0,0 @@
-let constrain=(n,t,a)=>Math.max(Math.min(n,a),t),map=(n,t,a,o,r,e)=>{const s=(n-t)/(a-t)*(r-o)+o;return e?o<r?constrain(s,o,r):constrain(s,r,o):s},normal=()=>{let n=window.scrollY,t=Math.abs(n%200-100)-100,a=Math.abs(Math.floor(map(t,0,100,0,8)));document.querySelector("#sprite-container").style.backgroundPosition=`-${400*a}px`};window.addEventListener("scroll",normal);
-//# sourceMappingURL=index.0c7be86b.js.map
--- docs/index.ee91b0a4.js
+++
@@ -1,2 +0,0 @@
-
-//# sourceMappingURL=index.ee91b0a4.js.map
