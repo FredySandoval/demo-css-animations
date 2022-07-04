488ade3633515917960ba180a7832c0570695ea3
Fredy Sandoval <shockerovip@gmail.com>
2022-07-03 19:10:47 -0600

    third commit

---
+++ docs/index.30e5fbe7.js
@@ -0,0 +1,2 @@
+let x=0;function changeY(){let e=window.scrollY;x=e;let l=document.querySelector(".squares>.layer:nth-child(4)");null!==l&&(l.style.webkitFilter=`hue-rotate(${x}deg)`)}window.addEventListener("scroll",changeY);
+//# sourceMappingURL=index.30e5fbe7.js.map
--- docs/index.a630069f.js
+++
@@ -1,2 +0,0 @@
-let x=0;function changeY(){let e=window.scrollY;x=e;let l=document.querySelector(".squares>.layer:nth-child(4)");null!==l&&(l.style.webkitFilter=`hue-rotate(${x}deg)`)}window.addEventListener("scroll",changeY);
-//# sourceMappingURL=index.a630069f.js.map
